22712f57dd75c55b184cf3479491b76a3219c640
maryrosedevega <143856348+maryrosedevega@users.noreply.github.com>
2023-12-19 23:40:01 +0800

    Delete RNA_seq directory

--- RNA_seq/bin/get_markov_order.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gawk '{ l=length($2); L+=l; print l} END{ print "TOTAL: "L;}' all.cds.tbl
-#800,322  (markov5)
-
-gawk '{ l=length($2); L+=l; print l} END{ print "TOTAL: "L;}' all.intron.tbl
-
